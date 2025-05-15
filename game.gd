a8b0dcc234b9374d225aa4a135ffab6eb77cb5de
Beeka <beekayt274@gmail.com>
2025-05-15 15:32:01 +1200

    Load and unload chunks by camera position

--- game.gd
+++ game.gd
@@ -1,17 +1,40 @@
 extends Node3D
 
 func _ready() -> void:
-	$"scene".loadChunk(Vector2(0,0))
+	updateCamera()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-			$"camera".position -= Vector3(event.relative.x, 0, event.relative.y) * $"camera".position.y * 0.0024
+			$"camera".position -= Vector3(event.relative.x, 0, event.relative.y) * $"camera".position.y * 0.00237
+			updateCamera()
 	elif event is InputEventMouseButton:
 		if event.is_pressed():
 			# zoom in
 			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
 				$"camera".position.y *= 0.8
+				updateCamera()
 			# zoom out
-			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and $"camera".position.y < 100:
 				$"camera".position.y *= 1.25
+				updateCamera()
+
+
+func updateCamera() -> void:
+	# 2y tan 37.5 * 1152/648
+	# it did say it was the horizontal fov but i guess it lied
+	var screenWidth:float = $"camera".position.y * 2.728273735
+	var screenHeight:float = screenWidth * 0.5625
+	
+	var leftBound:int = floor(($"camera".position.x - 0.5*screenWidth) / Scene.CHUNK_SIZE)
+	var rightBound:int = ceil(($"camera".position.x + 0.5*screenWidth) / Scene.CHUNK_SIZE)
+	var topBound:int = floor(($"camera".position.z - 0.5*screenHeight) / Scene.CHUNK_SIZE)
+	var bottomBound:int = ceil(($"camera".position.z + 0.5*screenHeight) / Scene.CHUNK_SIZE)
+	for chunkPosition:Vector2i in $"scene".chunkPositions.duplicate():
+		if chunkPosition.x < leftBound or chunkPosition.x >= rightBound or chunkPosition.y < topBound or chunkPosition.y >= bottomBound:
+			$"scene".unloadChunk(chunkPosition)
+	for x:int in range(leftBound, rightBound):
+		for y:int in range(topBound, bottomBound):
+			var thisChunkPosition:Vector2i = Vector2i(x, y)
+			if thisChunkPosition not in $"scene".chunkPositions:
+				$"scene".loadChunk(thisChunkPosition)
--- scene.gd
+++ scene.gd
@@ -1,9 +1,19 @@
 extends Node3D
+class_name Scene
 const CHUNK = preload("res://chunk.tscn");
+const CHUNK_SIZE:float = 32
 
-var chunks:Array[Chunk]
+var chunks:Array[Chunk] = []
+var chunkPositions:Array[Vector2i] = []
 
-func loadChunk(chunkPos:Vector2) -> void:
-	var chunk = CHUNK.instantiate()
-	chunk.position = Vector3(chunkPos.x * 32, 0, chunkPos.y * 32)
+func loadChunk(chunkPos:Vector2i) -> void:
+	var chunk:Chunk = CHUNK.instantiate()
+	chunk.position = Vector3(chunkPos.x * 32 + 16, 0, chunkPos.y * 32 + 16)
+	chunks.append(chunk)
+	chunkPositions.append(chunkPos)
 	add_child(chunk)
+
+func unloadChunk(chunkPos:Vector2i) -> void:
+	var index:int = chunkPositions.find(chunkPos)
+	chunks.pop_at(index).queue_free()
+	chunkPositions.pop_at(index)
